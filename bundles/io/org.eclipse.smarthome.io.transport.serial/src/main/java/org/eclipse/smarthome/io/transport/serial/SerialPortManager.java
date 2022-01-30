635bb07083835e5017bc5a3cf5923933c8356ad0
PtrO <34420738+ptrooms@users.noreply.github.com>
2022-01-30 20:54:09 +0100

    Re-add serial modules wich were taken out

--- bundles/io/org.eclipse.smarthome.io.transport.serial/src/main/java/org/eclipse/smarthome/io/transport/serial/SerialPortManager.java
+++ bundles/io/org.eclipse.smarthome.io.transport.serial/src/main/java/org/eclipse/smarthome/io/transport/serial/SerialPortManager.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014,2019 Contributors to the Eclipse Foundation
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -38,6 +38,7 @@ public interface SerialPortManager {
         } else {
             return null;
         }
+        // return getIdentifiers().filter(id -> id.getName().equals(name)).findFirst().orElse(null);
     }
 
     /**
